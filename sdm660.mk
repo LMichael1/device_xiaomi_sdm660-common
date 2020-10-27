214197d95537324d55287c45a0ea0fd8181ff6e0
Fedor Shatokhin <groza01i@gmail.com>
2020-10-27 20:49:05 +0500

    sdm660-common: Decommonize ir
    Signed-off-by: Fedor Shatokhin <groza01i@gmail.com>

--- sdm660.mk
+++ sdm660.mk
@@ -178,9 +178,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.configstore@1.0-service
 
-PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/android.hardware.consumerir.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.consumerir.xml
-
 # Component overrides
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/component-overrides.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sysconfig/component-overrides.xml \
