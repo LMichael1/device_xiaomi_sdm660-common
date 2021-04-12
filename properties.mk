9177d5f6516092e9094b0e950997b7ab134435a9
Eric Farias <erikofarias@gmail.com>
2021-04-13 01:36:26 +0200

    Revert "sdm660-common: Disable WiFi display for now"
    This reverts commit d411e3b057cacf92b3c378e750a03d2ebb28639b.
    
    Change-Id: I3142b6fb9033efd5da3b18345fd87a146691bc50

--- properties.mk
+++ properties.mk
@@ -200,7 +200,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # WFD
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
-    persist.debug.wfd.enable=0
+    persist.debug.wfd.enable=1
 
 # IORap
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
